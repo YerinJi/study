d36f2eee19de4f9606f7380f189af35e7f122643
yerin_J <86339431+jiyerin@users.noreply.github.com>
2023-07-26 22:55:41 +0900

    .

--- lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/clock.js
+++ lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/clock.js
@@ -7,6 +7,5 @@ function getClock() {
     const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = `${housrs}:${minutes}:${seconds}`;
 }
-
 getClock();
 setInterval(getClock, 1000);
