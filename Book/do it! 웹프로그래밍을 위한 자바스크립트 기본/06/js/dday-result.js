a98f0dc1671303097e80e7202bdcf0eed98cab95
yerinJi <yerin6428@gmail.com>
2024-01-17 19:22:17 +0900

    HTML,CSS,JS : 기념일 계산하기

---
+++ Book/do it! 웹프로그래밍을 위한 자바스크립트 기본/06/js/dday-result.js
@@ -0,0 +1,20 @@
+
+var now = new Date(); 
+// 오늘 날짜 정보를 Date객체의 인스턴스 now 객체로 만든다.
+var firstDay = new Date("2018-03-23");
+// 처음 만난 날짜 정보를 firstDay 객체로 만든다.
+
+var toNow = now.getTime(); //오늘 날짜를 밀리초로 바꿈
+var toFirst = firstDay.getTime(); //처음 만난 날을 밀리초로 바꿈
+var passedTime = toNow - toFirst; //처음 만난 날과 오늘 사이의 차이 (밀리초 값)
+
+var passedDay = Math.round(passedTime/(1000*60*60*24)); //밀리초를 날짜 수로 변환한 후 반올림
+
+document.querySelector('#accent').innerHTML = passedDay + "일";
+
+var future = toFirst + 100 * (1000*60*60*24); //처음 만난 날에 100일 더함
+var someday = new Date(future); //future 값을 사용해 Date 객체의 인스턴스를 만든다.
+var year = someday.getFullYear();
+var month = someday.getMonth();
+var date = someday.getDate();
+document.querySelector('#date100').innerHTML = year + "년 " +month +"월 " + date + "일" ;
