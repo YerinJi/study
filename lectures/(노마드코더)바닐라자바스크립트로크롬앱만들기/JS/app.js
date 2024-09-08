1081a57dc2168c9d8fc8a559fa445f36d8b66482
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-09-08 13:26:08 +0900

    #4.4 getting username

--- lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
+++ lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
@@ -1,17 +1,18 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault(); //이벤트의 기본 행동이 발생되지 않도록 막는 것. preventDefault
-  console.log("Hello " + loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerHTML = `Hello ${username}`;
+  //greeting.innerHTML = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
