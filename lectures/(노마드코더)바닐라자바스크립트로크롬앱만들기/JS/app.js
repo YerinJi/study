c3476e92467948661af8e25bf64443c5ea59a2df
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-09-08 13:15:25 +0900

    #4.3 Events part Two

--- lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
+++ lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
@@ -1,9 +1,17 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault(); //이벤트의 기본 행동이 발생되지 않도록 막는 것. preventDefault
   console.log("Hello " + loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
