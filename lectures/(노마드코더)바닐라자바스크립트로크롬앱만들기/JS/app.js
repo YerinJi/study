2ec256aabd504299dbbd9e2ca7ce5b5cb4bad54a
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-09-08 13:52:37 +0900

    #4.7 super recap

--- lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
+++ lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/app.js
@@ -3,6 +3,7 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const link = document.querySelector("a");
 
@@ -10,10 +11,22 @@ function onLoginSubmit(event) {
   event.preventDefault(); //이벤트의 기본 행동이 발생되지 않도록 막는 것. preventDefault
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerHTML = `Hello ${username}`;
-  //greeting.innerHTML = "Hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+  greeting.innerHTML = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
