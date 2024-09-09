928c2b7910fa3bf596de1f585521f858ff111849
yerinnn_J <86339431+YerinJi@users.noreply.github.com>
2024-09-09 23:10:16 +0900

    #6.2 recap

---
+++ lectures/(노마드코더)바닐라자바스크립트로크롬앱만들기/JS/background.js
@@ -0,0 +1,11 @@
+const images = ["2.png", "3.png", "1.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage);
