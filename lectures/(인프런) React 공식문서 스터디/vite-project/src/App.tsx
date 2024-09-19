5078a695e6d569d264904612ac89c981dc51439b
yerinJi <yerin6428@gmail.com>
2024-09-19 19:51:26 +0900

    lifting state up

--- lectures/(인프런) React 공식문서 스터디/vite-project/src/App.tsx
+++ lectures/(인프런) React 공식문서 스터디/vite-project/src/App.tsx
@@ -1,68 +1,35 @@
 import { useState } from "react";
 
-// const user = {
-//   name: "Hedy Lamarr",
-//   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-//   imageSize: 90,
-// };
-
-// export default function Profile() {
-//   return (
-//     <>
-//       <h1>{user.name}</h1>
-//       <img
-//         className="avatar"
-//         src={user.imageUrl}
-//         alt={"Photo of " + user.name}
-//         style={{
-//           width: user.imageSize,
-//           height: user.imageSize,
-//         }}
-//       />
-//     </>
-//   );
-// }
-
-// const products = [
-//   { title: "Cabbage", isFruit: false, id: 1 },
-//   { title: "Garlic", isFruit: false, id: 2 },
-//   { title: "Apple", isFruit: true, id: 3 },
-// ];
-
-// export default function ShoppingList() {
-//   const listItems = products.map((product) => (
-//     <li
-//       key={product.id}
-//       style={{
-//         color: product.isFruit ? "magenta" : "darkgreen",
-//       }}
-//     >
-//       {product.title}
-//     </li>
-//   ));
-//   return <ul>{listItems}</ul>;
-// }
-
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick() {
-    setCount(count + 1);
+    setValue("X");
   }
   return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
   );
 }
 
-interface Props {
-  count: number;
-  onClick: () => void;
-}
-
-function MyButton({ count, onClick }: Props) {
-  return <button onClick={onClick}>Clicked {count} times</button>;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
