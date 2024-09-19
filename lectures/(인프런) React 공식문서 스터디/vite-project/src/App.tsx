376f0c5a01705902bc378b5570e39819eae025a6
yerinJi <yerin6428@gmail.com>
2024-09-19 13:09:02 +0900

    quick start

--- lectures/(인프런) React 공식문서 스터디/vite-project/src/App.tsx
+++ lectures/(인프런) React 공식문서 스터디/vite-project/src/App.tsx
@@ -1,10 +1,68 @@
 import { useState } from "react";
 
-function App() {
-  const [liked, setLiked] = useState(false);
-  if (liked) return "you liked this";
+// const user = {
+//   name: "Hedy Lamarr",
+//   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+//   imageSize: 90,
+// };
 
-  return <button onClick={() => setLiked(true)}>클릭!</button>;
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={"Photo of " + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize,
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+// const products = [
+//   { title: "Cabbage", isFruit: false, id: 1 },
+//   { title: "Garlic", isFruit: false, id: 2 },
+//   { title: "Apple", isFruit: true, id: 3 },
+// ];
+
+// export default function ShoppingList() {
+//   const listItems = products.map((product) => (
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? "magenta" : "darkgreen",
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   ));
+//   return <ul>{listItems}</ul>;
+// }
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
 }
 
-export default App;
+interface Props {
+  count: number;
+  onClick: () => void;
+}
+
+function MyButton({ count, onClick }: Props) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
